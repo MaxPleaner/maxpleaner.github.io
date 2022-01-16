51e842d653c244df4057f83ab4a14524696e989d
max pleaner <max.pleaner@hover.to>
2022-01-16 13:00:20 -0800

    add stuff

--- build.rb
+++ build.rb
@@ -29,22 +29,23 @@ class SiteBuilder
 		FileUtils.rm_rf("./dist/.", secure: true)
 		FileUtils.mkdir_p("./dist/public")
 
-		Dir.glob("./src/*.slim").each do |src|
-			dest = src.gsub("/src/", "/dist/").gsub(".slim", ".html")
-			render_slim(src, dest)
-			puts "built #{src}"
-		end
-
-		Dir.glob("./src/public/**/*").each do |src|
-			dest = src.gsub("/src/", "/dist/")
-			if File.directory?(src)
-				folder = dest.split("/")[0..-1].join("/")
-				FileUtils.mkdir_p(folder)
-			else
-				FileUtils.copy(src, dest) rescue binding.pry
+		["./src/pages", "./src/public"].each do |dir_prefix|
+			Dir.glob("#{dir_prefix}/**/*").each do |src|
+				dest = src.gsub("/src/", "/dist/").gsub("/pages/", "/")
+				if File.directory?(src)
+					FileUtils.mkdir_p(dest)
+				else
+					if dest.end_with?(".slim")
+						dest.gsub!(".slim", ".html")
+						render_slim(src, dest)
+					else
+						FileUtils.copy(src, dest)
+					end
+				end
 			end
-			puts "built #{src}"
 		end
+		
+		puts "done"
 	end
 end
 
