59a984018a108d09ed648159ff4138d4faec27c1
Max Pleaner <maxpleaner@gmail.com>
2022-04-20 08:39:08 -0700

    Update build.rb

--- build.rb
+++ build.rb
@@ -3,52 +3,61 @@ require 'fileutils'
 require 'pry'
 
 class SiteBuilder
-	PartialData = {}
+  PartialData = {}
 
-	def self.set_partial_data(**data, &blk)
-		PartialData.merge!(data)
-		result = blk.call
-		PartialData.clear
-		result
-	end
+  # Main entry point. Compiles src/ to dest/
+  def self.run
+    FileUtils.rm_rf("./dist/.", secure: true)
+    FileUtils.mkdir_p("./dist/public")
 
-	def self.partial(src, **data)
-		set_partial_data(**data) do
-			Tilt.new(src, {pretty: true}).render(self, data)
-		end
-	end
+    ["./src/pages", "./src/public"].each do |dir_prefix|
+      Dir.glob("#{dir_prefix}/**/*").each do |src|
+        dest = src.gsub("/src/", "/dist/").gsub("/pages/", "/")
+        if File.directory?(src)
+          FileUtils.mkdir_p(dest)
+        else
+          if dest.end_with?(".slim")
+            dest.gsub!(".slim", ".html")
+            render_slim(src, dest)
+          else
+            FileUtils.copy(src, dest)
+          end
+        end
+      end
+    end
 
-	def self.render_slim(src, dest, **data)
-		File.open(dest, "w") do |dest_file|
-			rendered_html = partial(src, **data)
-			dest_file.write(rendered_html)
-		end
-	end
+    puts "~~~ BUILT SITE ~~~"
+  end
 
-	def self.run
-		FileUtils.rm_rf("./dist/.", secure: true)
-		FileUtils.mkdir_p("./dist/public")
+  # Call this from a view to render another sub view. Second argument is optional partial data e.g. { foo: "bar" }
+  def self.partial(src, **data)
+    set_partial_data(**data) do
+      Tilt.new(src, {pretty: true}).render(self, data)
+    end
+  end
 
-		["./src/pages", "./src/public"].each do |dir_prefix|
-			Dir.glob("#{dir_prefix}/**/*").each do |src|
-				dest = src.gsub("/src/", "/dist/").gsub("/pages/", "/")
-				if File.directory?(src)
-					FileUtils.mkdir_p(dest)
-				else
-					if dest.end_with?(".slim")
-						dest.gsub!(".slim", ".html")
-						render_slim(src, dest)
-					else
-						FileUtils.copy(src, dest)
-					end
-				end
-			end
-		end
-		
-		# puts "~~~ BUILT SITE ~~~"
-	end
+  class << self
+    private
+
+    # Internal method which populates PartialData, calls the block, then empties PartialData
+    # This script is single-threaded so using a global works fine. 
+    def set_partial_data(**data, &blk)
+      PartialData.merge!(data)
+      result = blk.call
+      PartialData.clear
+      result
+    end
+
+    # Compiles `src` slim file to html and writes to `dest`
+    def render_slim(src, dest, **data)
+      File.open(dest, "w") do |dest_file|
+        rendered_html = partial(src, **data)
+        dest_file.write(rendered_html)
+      end
+    end
+  end
 end
 
-# if __FILE__ == $0
-	SiteBuilder.run
-# end
+if __FILE__ == $0
+  SiteBuilder.run
+end
